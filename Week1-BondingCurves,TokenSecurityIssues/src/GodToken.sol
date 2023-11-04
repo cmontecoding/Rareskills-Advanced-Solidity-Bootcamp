5c42ea997284351c113114e4c830f25b30b671f6
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-11-04 15:30:49 -0400

    👷✅  add and test GodToken

---
+++ Week1-BondingCurves,TokenSecurityIssues/src/GodToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+///@notice Token with god mode. A special address is able
+/// to transfer tokens between addresses at will.
+contract GodToken is Ownable, ERC20 {
+
+    constructor(
+        uint256 _totalSupply
+    ) Ownable(msg.sender) ERC20("GodToken", "GODT") {
+        _mint(msg.sender, _totalSupply);
+    }
+
+    /// @notice Owner can transfer tokens between addresses
+    function godTransfer(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external onlyOwner {
+        _transfer(_from, _to, _amount);
+    }
+
+}
---
+++ Week1-BondingCurves,TokenSecurityIssues/test/GodToken.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {GodToken} from "../src/GodToken.sol";
+
+contract GodTokenTest is Test {
+    GodToken god;
+    address admin;
+    address user1;
+    address user2;
+
+    function setUp() public {
+        admin = address(this);
+        user1 = address(0x1);
+        user2 = address(0x2);
+        god = new GodToken(1000);
+        god.transfer(user1, 100);
+        god.transfer(user2, 100);
+
+        ///@dev make sure initializations are correct
+        assertEq(god.totalSupply(), 1000);
+        assertEq(god.balanceOf(admin), 800);
+        assertEq(god.balanceOf(user1), 100);
+        assertEq(god.balanceOf(user2), 100);
+    }
+
+    /// @notice Test that the owner can transfer tokens between addresses
+    function testBlacklistSender() public {
+        god.godTransfer(user1, user2, 10);
+        assertEq(god.balanceOf(user1), 90);
+        assertEq(god.balanceOf(user2), 110);
+    }
+
+}
