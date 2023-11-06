27bb1855fea22373146756ad24a695403688b4b6
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-11-05 19:22:30 -0500

    👷✅ add and test UntrustedEscrow

---
+++ Week1-BondingCurves,TokenSecurityIssues/src/UntrustedEscrow.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+///@notice Untrusted escrow. Create a contract where a buyer can put an arbitrary
+/// ERC20 token into a contract and a seller can withdraw it 3 days later.
+/// Based on your readings above, what issues do you need to defend against?
+/// Create the safest version of this that you can while guarding against issues that you cannot control.
+/// Does your contract handle fee-on transfer tokens or non-standard ERC20 tokens.
+contract UntrustedEscrow is Ownable {
+    address public buyer;
+    address public seller;
+    IERC20 public token;
+    uint256 public depositTime;
+    uint256 public constant WITHDRAW_DELAY = 3 days;
+    bool public deposited;
+
+    constructor() Ownable(msg.sender) {}
+
+    function initialize(
+        address _buyer,
+        address _seller,
+        IERC20 _token
+    ) external onlyOwner {
+        require(buyer == address(0), "Contract is already initialized");
+        buyer = _buyer;
+        seller = _seller;
+        token = _token;
+    }
+
+    function deposit(uint256 amount) external {
+        require(msg.sender == buyer, "Only the buyer can deposit");
+        require(!deposited, "You have already deposited");
+        deposited = true;
+        depositTime = block.timestamp;
+
+        /// @dev require the transferFrom to make sure the transaction went through
+        require(
+            token.transferFrom(buyer, address(this), amount),
+            "Token transfer failed"
+        );
+    }
+
+    function withdraw() external {
+        require(msg.sender == seller, "Only the seller can withdraw");
+        require(depositTime != 0, "Nothing is deposited yet");
+        require(
+            block.timestamp >= depositTime + WITHDRAW_DELAY,
+            "Withdrawal time has not arrived yet"
+        );
+        uint256 balance = token.balanceOf(address(this));
+
+        /// @dev require the transfer to make sure the transaction went through
+        require(token.transfer(seller, balance), "Token transfer failed");
+    }
+
+    /// @notice The Owner can recover the ERC20 token that was deposited
+    function recoverERC20(
+        address tokenAddress,
+        uint256 amount
+    ) external onlyOwner {
+        require(
+            tokenAddress != address(token),
+            "Cannot recover the deposited token"
+        );
+        IERC20(tokenAddress).transfer(owner(), amount);
+    }
+}
---
+++ Week1-BondingCurves,TokenSecurityIssues/test/UntrustedEscrow.t.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {UntrustedEscrow} from "../src/UntrustedEscrow.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+contract UntrustedEscrowTest is Test {
+    UntrustedEscrow untrusted;
+    address admin;
+    address user1;
+    address user2;
+    DummyERC20 token;
+
+    function setUp() public {
+        /// @dev warp forward so that block timestamp is not 0
+        vm.warp(block.timestamp+1000);
+
+        admin = address(this);
+        user1 = address(0x1);
+        user2 = address(0x2);
+        untrusted = new UntrustedEscrow();
+        token = new DummyERC20("Test", "TEST");
+        token.mint(user1, 10);
+    }
+
+    function testInitialize() public {
+        untrusted.initialize(user1, user2, IERC20(address(token)) 
+        );
+
+        assertEq(untrusted.buyer(), user1);
+        assertEq(untrusted.seller(), user2);
+        assertEq(address(untrusted.token()), address(token));
+    }
+
+    function testDeposit() public {
+        untrusted.initialize(user1, user2, IERC20(address(token)) 
+        );
+
+        vm.startPrank(user1);
+        token.approve(address(untrusted), 10);
+        untrusted.deposit(10);
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(address(untrusted)), 10);
+        assertEq(untrusted.depositTime(), block.timestamp);
+        assertEq(untrusted.deposited(), true);
+    }
+
+    function testWithdraw() public {
+        untrusted.initialize(user1, user2, IERC20(address(token)) 
+        );
+
+        vm.startPrank(user1);
+        token.approve(address(untrusted), 10);
+        untrusted.deposit(10);
+        vm.stopPrank();
+
+        vm.warp(block.timestamp + 3 days);
+        vm.startPrank(user2);
+        untrusted.withdraw();
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(address(untrusted)), 0);
+        assertEq(token.balanceOf(user2), 10);
+    }
+
+    function testWithdrawTooSoon() public {
+        untrusted.initialize(user1, user2, IERC20(address(token)) 
+        );
+
+        vm.startPrank(user1);
+        token.approve(address(untrusted), 10);
+        untrusted.deposit(10);
+        vm.stopPrank();
+
+        vm.warp(block.timestamp + 2 days);
+        vm.startPrank(user2);
+        vm.expectRevert("Withdrawal time has not arrived yet");
+        untrusted.withdraw();
+    }
+
+}
+
+contract DummyERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
