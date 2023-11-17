a8f65b8a89e76dafae528ba53a3f26b8662586c4
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-11-17 10:08:38 -0500

    👷 add unchecked to for loops

--- Week2-NFTVariants,Staking/src/Ecosystem2/PrimeNFTs.sol
+++ Week2-NFTVariants,Staking/src/Ecosystem2/PrimeNFTs.sol
@@ -13,13 +13,16 @@ contract PrimeNFTs {
     function countPrimeNFTs(address owner) external view returns (uint256) {
         uint256 balance = nftCollection.balanceOf(owner);
         uint256 primeCount = 0;
-        for (uint256 tokenIds = 0; tokenIds < balance; tokenIds++) {
+        for (uint256 tokenIds = 0; tokenIds < balance; ) {
             uint256 tokenId = nftCollection.tokenOfOwnerByIndex(
                 owner,
                 tokenIds
             );
             if (isPrime(tokenId)) {
                 primeCount++;
+            }
+            unchecked {
+                tokenIds++;
             }
         }
 
@@ -41,9 +44,12 @@ contract PrimeNFTs {
         }
         /// @dev all primes are of the form 6k ± 1, with the exception of 2 and 3
         /// @dev this checks if the number is divisible by i or i + 2 for each increment of 6
-        for (uint256 i = 5; i * i <= number; i += 6) {
+        for (uint256 i = 5; i * i <= number; ) {
             if (number % i == 0 || number % (i + 2) == 0) {
                 return false;
+            }
+            unchecked {
+                i += 6;
             }
         }
         return true;
