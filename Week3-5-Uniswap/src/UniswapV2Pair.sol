1d9788eae60d8c9e3f4df2302e8778325775ba3e
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-11-27 12:14:56 -0500

    change sqrt to solady

--- Week3-5-Uniswap/src/UniswapV2Pair.sol
+++ Week3-5-Uniswap/src/UniswapV2Pair.sol
@@ -1,11 +1,11 @@
 pragma solidity 0.8.21;
 
 import './interfaces/IUniswapV2Pair.sol';
-import './libraries/Math.sol';
 import './libraries/UQ112x112.sol';
 import './interfaces/IUniswapV2Factory.sol';
 import './interfaces/IUniswapV2Callee.sol';
 import {ERC20} from "solady/src/tokens/ERC20.sol";
+import {FixedPointMathLib} from "solady/src/utils/FixedPointMathLib.sol";
 
 contract UniswapV2Pair is IUniswapV2Pair, ERC20 {
     using UQ112x112 for uint224;
@@ -86,8 +86,8 @@ contract UniswapV2Pair is IUniswapV2Pair, ERC20 {
         uint _kLast = kLast; // gas savings
         if (feeOn) {
             if (_kLast != 0) {
-                uint rootK = Math.sqrt(uint(_reserve0) * (_reserve1));
-                uint rootKLast = Math.sqrt(_kLast);
+                uint rootK = FixedPointMathLib.sqrt(uint(_reserve0) * (_reserve1));
+                uint rootKLast = FixedPointMathLib.sqrt(_kLast);
                 if (rootK > rootKLast) {
                     uint numerator = totalSupply() * (rootK - (rootKLast));
                     uint denominator = rootK * (5) + (rootKLast);
@@ -111,10 +111,10 @@ contract UniswapV2Pair is IUniswapV2Pair, ERC20 {
         bool feeOn = _mintFee(_reserve0, _reserve1);
         uint _totalSupply = totalSupply(); // gas savings, must be defined here since totalSupply can update in _mintFee
         if (_totalSupply == 0) {
-            liquidity = Math.sqrt(amount0 * (amount1)) - (MINIMUM_LIQUIDITY);
+            liquidity = FixedPointMathLib.sqrt(amount0 * (amount1)) - (MINIMUM_LIQUIDITY);
            _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
         } else {
-            liquidity = Math.min(amount0 * (_totalSupply) / _reserve0, amount1 * (_totalSupply) / _reserve1);
+            liquidity = FixedPointMathLib.min(amount0 * (_totalSupply) / _reserve0, amount1 * (_totalSupply) / _reserve1);
         }
         require(liquidity > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
         _mint(to, liquidity);
