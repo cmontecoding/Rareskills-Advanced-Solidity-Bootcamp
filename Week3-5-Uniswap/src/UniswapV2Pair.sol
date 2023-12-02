32906c33497c84b747092a7e4282f88f98403a16
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-12-01 21:16:54 -0500

    👷 fix uint112.max in _update

--- Week3-5-Uniswap/src/UniswapV2Pair.sol
+++ Week3-5-Uniswap/src/UniswapV2Pair.sol
@@ -59,7 +59,7 @@ contract UniswapV2Pair is IUniswapV2Pair, ERC20, ReentrancyGuard, IERC3156FlashL
 
     // update reserves and, on the first call per block, price accumulators
     function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
-        require(balance0 <= 2^112 - 1 && balance1 <= 2^112 - 1, 'UniswapV2: OVERFLOW');
+        require(balance0 <= type(uint112).max && balance1 <= type(uint112).max, 'UniswapV2: OVERFLOW');
         uint32 blockTimestamp = uint32(block.timestamp % 2**32);
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
         if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
