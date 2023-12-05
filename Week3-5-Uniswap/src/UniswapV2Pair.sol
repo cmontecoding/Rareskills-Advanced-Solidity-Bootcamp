265bd53f1240c0a03eb3a853116cd24dfe3aafa1
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-12-05 17:41:17 -0500

    👷 add unchecked to _update

--- Week3-5-Uniswap/src/UniswapV2Pair.sol
+++ Week3-5-Uniswap/src/UniswapV2Pair.sol
@@ -89,13 +89,15 @@ contract UniswapV2Pair is
         uint32 blockTimestamp = uint32(block.timestamp % 2 ** 32);
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
         if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
-            // * never overflows, and + overflow is desired
-            price0CumulativeLast +=
-                uint(UQ112x112.encode(_reserve1).uqdiv(_reserve0)) *
-                timeElapsed;
-            price1CumulativeLast +=
-                uint(UQ112x112.encode(_reserve0).uqdiv(_reserve1)) *
-                timeElapsed;
+            unchecked {
+                // * never overflows, and + overflow is desired
+                price0CumulativeLast +=
+                    uint(UQ112x112.encode(_reserve1).uqdiv(_reserve0)) *
+                    timeElapsed;
+                price1CumulativeLast +=
+                    uint(UQ112x112.encode(_reserve0).uqdiv(_reserve1)) *
+                    timeElapsed;
+            }
         }
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
