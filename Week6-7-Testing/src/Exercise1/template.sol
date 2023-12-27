d651d9a4e2e4a6f1d7b76a44dd6c2b4273c81234
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-12-27 18:55:27 -0500

    exercise 1

---
+++ Week6-7-Testing/src/Exercise1/template.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import "./token.sol";
+
+/// @dev Run the template with
+///      ```
+///      solc-select use 0.8.0
+///      echidna program-analysis/echidna/exercises/exercise1/template.sol
+///      ```
+contract TestToken is Token {
+    address echidna = tx.origin;
+
+    constructor() {
+        balances[echidna] = 10_000;
+    }
+
+    function echidna_test_balance() public view returns (bool) {
+        return balances[echidna] == 10_000;
+    }
+}
---
+++ Week6-7-Testing/src/Exercise1/token.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address public owner = msg.sender;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable: Caller is not the owner.");
+        _;
+    }
+}
+
+contract Pausable is Ownable {
+    bool private _paused;
+
+    function paused() public view returns (bool) {
+        return _paused;
+    }
+
+    function pause() public onlyOwner {
+        _paused = true;
+    }
+
+    function resume() public onlyOwner {
+        _paused = false;
+    }
+
+    modifier whenNotPaused() {
+        require(!_paused, "Pausable: Contract is paused.");
+        _;
+    }
+}
+
+contract Token is Ownable, Pausable {
+    mapping(address => uint256) public balances;
+
+    function transfer(address to, uint256 value) public whenNotPaused {
+        // unchecked to save gas
+        unchecked {
+            balances[msg.sender] -= value;
+            balances[msg.sender] += value;
+        }
+    }
+}
