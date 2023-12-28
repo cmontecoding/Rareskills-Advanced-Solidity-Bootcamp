8b757fefafc1f0bb0954f0914464afeef6c3a417
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-12-28 16:03:14 -0500

    start TokenWhale

---
+++ Week6-7-Testing/src/TokenWhale/solution.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.21;
+
+import "./TokenWhaleChallenge.sol";
+
+contract TestToken is TokenWhaleChallenge {
+    address echidna = msg.sender;
+
+    function TestToken() public {
+        TokenWhaleChallenge(msg.sender);
+    }
+
+    // constuctor() {
+    //     TokenWhaleChallenge(msg.sender);
+    // }
+
+    function echidna_test_balance() public view returns (bool) {
+        //return balanceOf[echidna] <= 1000;
+        return isComplete() == false;
+    }
+}
