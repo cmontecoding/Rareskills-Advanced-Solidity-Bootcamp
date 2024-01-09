c3f0bb1a10e6447bc7d18f2c6dba6dd205fac650
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2024-01-08 21:23:40 -0500

    👷 fix TokenWhaleChallenge

--- Week6-7-Testing/src/TokenWhale/solution.sol
+++ Week6-7-Testing/src/TokenWhale/solution.sol
@@ -1,19 +1,19 @@
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.4.21;
 
 import "./TokenWhaleChallenge.sol";
 
-contract TestToken is TokenWhaleChallenge {
+/// @dev Run the template with
+///      ```
+///      solc-select use 0.4.25 --always-install
+///      echidna src/TokenWhale/solution.sol --contract TokenWhaleChallengeTest
+///      ```
+contract TokenWhaleChallengeTest is TokenWhaleChallenge {
     address echidna = msg.sender;
 
-    function TestToken() public {
-        TokenWhaleChallenge(msg.sender);
-    }
-
-    // constuctor() {
-    //     TokenWhaleChallenge(msg.sender);
-    // }
+    function TokenWhaleChallengeTest() public TokenWhaleChallenge(msg.sender) {}
 
     function echidna_test_balance() public view returns (bool) {
-        return isComplete() == false;
+        return !isComplete();
     }
 }
