a4856277b8e5c908f1c906417978d0c8995d9e9b
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2024-01-13 14:36:09 -0500

    add rareskills riddles

---
+++ Week8-9:SecurityPart1/solidity-riddles/contracts/DeleteUser.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.8.15;
+
+/**
+ * This contract starts with 1 ether.
+ * Your goal is to steal all the ether in the contract.
+ *
+ */
+ 
+contract DeleteUser {
+    struct User {
+        address addr;
+        uint256 amount;
+    }
+
+    User[] private users;
+
+    function deposit() external payable {
+        users.push(User({addr: msg.sender, amount: msg.value}));
+    }
+
+    function withdraw(uint256 index) external {
+        User storage user = users[index];
+        require(user.addr == msg.sender);
+        uint256 amount = user.amount;
+
+        user = users[users.length - 1];
+        users.pop();
+
+        msg.sender.call{value: amount}("");
+    }
+}
